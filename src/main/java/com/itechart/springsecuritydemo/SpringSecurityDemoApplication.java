69023b4abbbb9bf248dcb68bdf1a7a8597966a1f
Alexey <alexey.severyn@gmail.com>
2025-08-12 07:19:48 +0300

    eureka client dependency added and basic properties set up

--- src/main/java/com/itechart/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/itechart/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -2,8 +2,10 @@ package com.itechart.springsecuritydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringSecurityDemoApplication {
 
     public static void main(String[] args) {
