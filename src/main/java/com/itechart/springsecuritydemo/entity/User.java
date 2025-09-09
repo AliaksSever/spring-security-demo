cb865b70c6ed258c412b08beebe48b35139c15cf
Alexey <alexey.severyn@gmail.com>
2025-09-09 11:49:19 +0300

    by default UserRepository findByUuid method return result of findByUuidWithRoles method which uses User.withRoles entityGraph

--- src/main/java/com/itechart/springsecuritydemo/entity/User.java
+++ src/main/java/com/itechart/springsecuritydemo/entity/User.java
@@ -18,14 +18,14 @@ import java.util.UUID;
 @Builder
 @Entity
 @Table(name = "users")
-@NamedEntityGraphs(
+@NamedEntityGraphs({
         @NamedEntityGraph(
                 name = "User.withRoles",
                 attributeNodes = {
                         @NamedAttributeNode("roles")
                 }
         )
-)
+})
 public class User {
 
     @Id
