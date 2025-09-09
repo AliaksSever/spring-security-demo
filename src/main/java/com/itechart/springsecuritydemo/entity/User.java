60809357e99d569a9c9a6034947517ea29e71202
Alexey <alexey.severyn@gmail.com>
2025-09-09 11:00:44 +0300

    N+1 problem for User and his Roles resolved:  - NamedEntityGraph User.withRoles

--- src/main/java/com/itechart/springsecuritydemo/entity/User.java
+++ src/main/java/com/itechart/springsecuritydemo/entity/User.java
@@ -18,6 +18,14 @@ import java.util.UUID;
 @Builder
 @Entity
 @Table(name = "users")
+@NamedEntityGraphs(
+        @NamedEntityGraph(
+                name = "User.withRoles",
+                attributeNodes = {
+                        @NamedAttributeNode("roles")
+                }
+        )
+)
 public class User {
 
     @Id
@@ -33,7 +41,7 @@ public class User {
     private String city;
     private String email;
 
-    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @ElementCollection(targetClass = Role.class)
     @Enumerated(EnumType.STRING)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     private Set<Role> roles;
